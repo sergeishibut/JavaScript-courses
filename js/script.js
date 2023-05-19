e311b4cfdea168dc8534350f8a86dd53f3ff6c29
Sergey <shibut.sergey@gmail.com>
2023-05-19 18:50:50 +0400

    movieDB

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+/* "use strict";
+
 const persone = "Alex";
 console.log(persone);
 
@@ -49,18 +51,18 @@ const storeDescription = {
 };
 console.log(storeDescription);
 
-/* alert("Hello"); */
+alert("Hello");
 
-/* const result = confirm("are you here?");
-console.log(result); */
+const result = confirm("are you here?");
+console.log(result);
 
-/* const answer = +prompt("are you 18", "18");
-console.log(answer + 5); */
+const answer = +prompt("are you 18", "18");
+console.log(answer + 5);
 
 const answeres = [];
-/* answeres[0] = prompt("ваше имя?", ""),
+answeres[0] = prompt("ваше имя?", ""),
 answeres[1] = prompt("ваше фамилия?", ""),
-answeres[2] = prompt("ваш возраст?", ""); */
+answeres[2] = prompt("ваш возраст?", "");
 
 console.log(typeof(answeres));
 
@@ -72,3 +74,35 @@ alert(`hello,${user}`);
 
 let incr = 10,
 	decr = 10;
+incr++;
+decr--;
+console.log(incr++);
+console.log(decr--);
+
+console.log(2 + 2 *2 );
+
+const isChecked = false,
+	isClose = false;
+console.log(isChecked || !isClose); */
+
+
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+const a = prompt("Latest watching movie?",""),
+	  b = prompt("Rate this movie?",""),
+	  c = prompt("Latest watching movie?",""),
+	  d = prompt("Rate this movie?","");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
